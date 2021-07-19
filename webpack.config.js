17ab9bf869df1e6bcac6f5e0a88d74e44921796b
kir <kir@localhost>
2021-07-19 15:09:42 -0400

    config, api gmaps to do: close tje key.

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,20 @@ const path = require('path');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
 
 var config = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx|ts|map)$/,
                 exclude: [/node_modules/],
                 use: [
                     {
                         loader: 'babel-loader',
-                        options: { presets: ['latest'] }
+                        options: { presets: ['@babel/preset-env'] }
+                    
                     }
                 ]
             },
@@ -33,7 +36,8 @@ var config = {
         new MiniCssExtractPlugin({
             filename: "[name].css",
             allChunks: true
-        })
+        }), 
+    	new Dotenv()
     ],
     optimization: {
         minimizer: [
@@ -49,6 +53,8 @@ var config = {
 }
 
 var frontendConfig = Object.assign({}, config, {
+	
+	mode: 'development',
     entry: {
         app: path.resolve(__dirname, './frontend/web/js/app.js'),
         style: path.resolve(__dirname, './frontend/web/css/style.less'),
@@ -59,7 +65,50 @@ var frontendConfig = Object.assign({}, config, {
     },
 });
 
+/*   "babel": {
+    "presets": [
+      "@babel/preset-env",
+      "@babel/preset-typescript",
+      "@babel/preset-react"
+    ],
+    "plugins": [
+      "@babel/proposal-class-properties",
+      "@babel/proposal-object-rest-spread"
+    ]
+  },
+var frontendReact = Object.assign({}, config, {
+    entry: {
+        app: path.resolve(__dirname, './node_modules/react/umd/react.production.min.js')
+    },
+    output: {
+        filename: '[name]-react.js',
+        path: path.resolve(__dirname, './frontend/web/bundle'),
+    },
+});
+
+var frontendReactDom = Object.assign({}, config, {
+    entry: {
+        app: path.resolve(__dirname, './node_modules/react-dom/umd/react-dom.production.min.js')
+    },
+    output: {
+        filename: '[name]-react-dom.js',
+        path: path.resolve(__dirname, './frontend/web/bundle'),
+    },
+});
+
+var frontendGMaps = Object.assign({}, config, {
+    entry: {
+    	app: path.resolve(__dirname, './node_modules/@react-google-maps/api/dist/reactgooglemapsapi.umd.production.min.js'),
+    },
+    output: {
+        filename: '[name]-gmaps.js',
+        path: path.resolve(__dirname, './frontend/web/bundle'),
+    },
+});
+*/
+
 var backendConfig = Object.assign({}, config, {
+	mode: 'development',
     entry: {
         app: path.resolve(__dirname, './backend/web/js/app.js'),
         style: path.resolve(__dirname, './backend/web/css/style.less'),
@@ -70,6 +119,8 @@ var backendConfig = Object.assign({}, config, {
     },
 });
 
+
 module.exports = [
-    frontendConfig, backendConfig
+    frontendConfig, backendConfig, /*frontendReact, frontendReactDom, frontendGMaps,*/
 ];
+
